6222a6e7a03dd8a361362b591fdf184f0442c64c
Gabriel Richter <gabrielrih@gmail.com>
2026-07-21 11:57:54 -0300

    Update change_owner.sql

--- pg/access/change_owner.sql
+++ pg/access/change_owner.sql
@@ -15,3 +15,13 @@ WHERE   schemaname NOT IN ('information_schema', 'pg_catalog', 'cron','pg_toast'
         tableowner <> 'new_owner_name' AND
         tablename NOT IN ('__EFMigrationsHistory') -- Table used by Azure PaaS
 ORDER BY schemaname, tablename;
+
+
+-- SEQUENCES
+-- Creates the ALTER SEQUENCE commands to change the sequence owner from all sequences from a specific db
+-- Example: ALTER SEQUENCE public."Area_id_seq" OWNER TO adminalmoxgo;
+SELECT 'ALTER SEQUENCE ' || schemaname || '."' || sequencename || '" OWNER TO new_owner_name;'
+FROM pg_sequences
+WHERE   schemaname NOT IN ('information_schema', 'pg_catalog', 'cron', 'pg_toast') AND
+  sequenceowner <> 'new_owner_name'
+ORDER BY schemaname, sequencename;
